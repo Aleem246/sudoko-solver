a55ba886e3882a93e98d32327455499fd744eb79
Aleem246 <21ag1a6640@gmail.com>
2025-02-04 13:19:23 +0530

    first commit

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-var arr = [[], [], [], [], [], [], [], [], []]
 
-for (var i = 0; i < 9; i++) {
-	for (var j = 0; j < 9; j++) {
-		arr[i][j] = document.getElementById(i * 9 + j);
+//speed reading'
+let speed = document.getElementById("speed").value	;
 
-	}
-}
-
+console.log(speed);
 
 var board = [[], [], [], [], [], [], [], [], []]
 
@@ -14,13 +10,13 @@ function FillBoard(board) {
 	for (var i = 0; i < 9; i++) {
 		for (var j = 0; j < 9; j++) {
 			if (board[i][j] != 0) {
-				arr[i][j].innerText = board[i][j]
+				document.getElementById(i*9+j).innerText = board[i][j];
 			}
-
+			
 			else
-				arr[i][j].innerText = ''
-		}
+			document.getElementById(i*9+j).innerText = '';
 	}
+}
 }
 
 let GetPuzzle = document.getElementById('GetPuzzle')
@@ -34,15 +30,76 @@ GetPuzzle.onclick = function () {
 		board = response.board
 		FillBoard(board)
 	}
-	xhrRequest.open('get', 'https://sugoku.herokuapp.com/board?difficulty=easy')
+	xhrRequest.open('get', 'https://sugoku.onrender.com/board?difficulty=easy')
 	//we can change the difficulty of the puzzle the allowed values of difficulty are easy, medium, hard and random
 	xhrRequest.send()
 }
 
 SolvePuzzle.onclick = () => {
-	SudokuSolver(board, 0, 0, 9);
+	speed = document.getElementById("speed").value;
+	SudokuSolver(board);
 };
 
-function SudokuSolver(board, i, j, n) {
-	// Write your Code here
+
+
+
+
+ function isValid(board,  ii, jj, num){
+	let sr = ii-ii%3;
+	let sc = jj-jj%3;
+
+	for(let i=0;i<3;i++){
+		for(let j=0;j<3;j++){
+			if(board[sr+i][sc+j]==num && sr+i!=ii && sc+j!=jj)
+				return false;
+		}
+	}
+	for(let i=0;i<9;i++){
+		if(board[i][jj]==num )
+			return false;
+	}
+	for(let i=0;i<9;i++){
+		if(board[ii][i]==num )
+			return false;
+	}
+	return true;
+}
+
+function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function SudokuSolver(board) {
+    
+
+    // Try to fill the cell with an appropriate number
+	for(let i=0;i< 9; i++){
+		for(let j=0;j<9;j++){
+			if(board[i][j]==0){
+				for (let num = 1; num <= 9; num++) {
+					// Check if num can be filled
+					if (isValid(board, i, j, num)) {
+						board[i][j] = num;
+						document.getElementById(i * 9 + j).innerText = num;
+						document.getElementById(i * 9 + j).style.backgroundColor = "white";
+						await wait(speed); // Add a delay of 0.3 seconds
+			
+						let subAns = await SudokuSolver(board);
+						if (subAns) {
+							return true;
+						}
+			
+						// Backtracking -> undo the change
+						board[i][j] = 0;
+						document.getElementById((i * 9) + j).style.backgroundColor="red";
+						await wait(speed); // Add a delay of 0.3 seconds
+					}
+				}
+				return false;
+			}
+		}
+	}
+    
+
+    return true;
 }
